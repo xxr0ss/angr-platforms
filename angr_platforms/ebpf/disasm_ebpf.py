7f0de04d16ca52d574bcd48a5a880e9bb946759c
xxr0ss <pjx3231@163.com>
2024-03-11 20:16:45 +0800

    register custom disassembly

--- angr_platforms/ebpf/disasm_ebpf.py
+++ angr_platforms/ebpf/disasm_ebpf.py
@@ -1,6 +1,10 @@
-from angr.block import DisassemblerBlock, DisassemblerInsn, register_disassembler
-from .arch_ebpf import ArchExtendedBPF
+from angr.block import DisassemblerBlock, DisassemblerInsn
+from angr.analyses import AnalysesHub, Disassembly
+from angr.analyses.disassembly import FuncComment, BlockStart, BlockNode, Hook
+from angr.codenode import BlockNode
 from .instrs_ebpf import EBPFInstruction
+import pyvex
+from pyvex.lifting.util import GymratLifter
 
 
 class EbpfBlock(DisassemblerBlock):
@@ -35,4 +39,35 @@ class EbpfInsn(DisassemblerInsn):
         return ", ".join(self.disasm[2])
 
 
-register_disassembler(ArchExtendedBPF, EbpfBlock, EbpfInsn)
+class EbpfDisassembly(Disassembly):
+    def parse_block(self, block: BlockNode) -> None:
+        """
+        Parse instructions for a given block node
+        """
+        func = self.func_lookup(block)
+        if func and func.addr == block.addr:
+            self.raw_result.append(FuncComment(block.function))
+            self.raw_result.append(func)
+        bs = BlockStart(block, func, self.project)
+        self.raw_result.append(bs)
+
+        if block.is_hook:
+            hook = Hook(block)
+            self.raw_result.append(hook)
+            self.raw_result_map["hooks"][block.addr] = hook
+        else:
+            # use our disassembly techs
+            self.block_to_insn_addrs[block.addr] = []
+            bytestr = self.project.factory.block(block.addr, size=block.size).bytes
+            gymrat_lifter: GymratLifter = pyvex.lifters[self.project.arch.name][0](self.project.arch, block.addr)
+            gymrat_lifter.lift(bytestr)
+            for insn in gymrat_lifter.decoded_insns:
+                disasm_insn = EbpfInsn(insn)
+                self._add_instruction_to_results(block, disasm_insn, bs)
+
+        if self._include_ir:
+            b = self.project.factory.block(block.addr, size=block.size)
+            self._add_block_ir_to_results(block, b.vex)
+
+
+AnalysesHub.register_default("Disassembly", EbpfDisassembly)
